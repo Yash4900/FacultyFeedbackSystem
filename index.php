768b3af5e40284354edf13fefe7c85e122035e89
Yash4900 <satra.y@somaiya.edu>
2020-03-18 13:45:51 +0530

    Favicon added

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ if(isset($_SESSION['role'])){
 	<title>FacultyFeedbackSystem</title>
 	<link rel="stylesheet" type="text/css" href="styles/index_style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+	<link rel="shortcut icon" type="image/png" href="images/logo.png">
+
 	<meta charset = "utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
